273309030fdc78c9e9500ea7695fd978b8bd4294
mohammad <m.deheghanifard@gmail.com>
2024-02-26 16:05:29 +0330

    Initial commit

---
+++ app/src/main/java/ir/dehghanifard/kotlin_retrofit2/MainActivity.kt
@@ -0,0 +1,46 @@
+package ir.dehghanifard.kotlin_retrofit2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ir.dehghanifard.kotlin_retrofit2.ui.theme.Kotlin_retrofit2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Kotlin_retrofit2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Kotlin_retrofit2Theme {
+        Greeting("Android")
+    }
+}
