a8b52166e6e72e1f404cccbc39230522fee03899
mohammad <m.deheghanifard@gmail.com>
2024-02-27 09:25:16 +0330

    impl retrofit config

--- app/src/main/java/ir/dehghanifard/kotlin_retrofit2/modules/ApiModule.kt
+++ app/src/main/java/ir/dehghanifard/kotlin_retrofit2/modules/ApiModule.kt
@@ -1,15 +1,20 @@
 package ir.dehghanifard.kotlin_retrofit2.modules
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import ir.dehghanifard.kotlin_retrofit2.config.getUnsafeOkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
+
+    @Provides
+    @Singleton
     fun provideRetrofit() : Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://onlineshop.holosen.net:9090")
